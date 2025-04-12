c97ce18fe0c9440f7d4ca0ead1238c811dfd7a0d
ankitsinghruhal <ruhalankitsingh@gmail.com>
2025-04-12 21:13:06 +0530

    commit

--- lms/src/com/application/frame/Settings.java
+++ lms/src/com/application/frame/Settings.java
@@ -7,5 +7,19 @@ import java.sql.DriverManager;
 
 public class Settings
 {
+    public static Dimension getScreenSize(){
+        Dimension d=Toolkit.getDefaultToolkit().getScreenSize();
+        return d;   
+    }//getScreenSize() closed
     
+    public static Connection getDBConnection(){
+        Connection con;
+        try{
+            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+            con=DriverManager.getConnection("jdbc:odbc:student");
+            return con;
+        }catch(Exception ex){
+            return null;
+        }
+    }
 }
