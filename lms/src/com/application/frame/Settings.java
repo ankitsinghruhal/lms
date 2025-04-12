efe0d07e34f226b74cd62f7c048488e8795da2ad
ankitsinghruhal <ruhalankitsingh@gmail.com>
2025-04-12 21:11:34 +0530

    commit

--- lms/src/com/application/frame/Settings.java
+++ lms/src/com/application/frame/Settings.java
@@ -1,5 +1,10 @@
 package com.application.frame;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Settings
 {
     public static void main(String[] args) {
