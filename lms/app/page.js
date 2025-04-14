a5f9147e2fcfe601b1b210d44abd65c92b52d23e
ankitsinghruhal <ruhalankitsingh@gmail.com>
2025-04-14 08:30:15 +0530

    initial setup

--- lms/app/page.js
+++ lms/app/page.js
@@ -1,9 +1,15 @@
+import { Button } from "@/components/ui/button";
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       Hello World !!!
+      <Button>Button</Button>
+
+      <LoginLink>Sign in</LoginLink>
+      <RegisterLink>Sign up</RegisterLink>
     </div>
   );
 }
